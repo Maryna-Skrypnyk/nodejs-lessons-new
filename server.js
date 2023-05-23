9f263814851159a42950b278a6ad104586cd5102
Maryna Skrypnyk <74303774+Maryna-Skrypnyk@users.noreply.github.com>
2023-05-23 22:22:30 +0300

    adds config

--- server.js
+++ server.js
@@ -1,15 +1,14 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
 const app = require("./app");
+const { DB_HOST } = require("./config");
 
 mongoose.set("strictQuery", true);
 
 const PORT = process.env.PORT || 3000;
 
 mongoose
-  .connect(
-    "mongodb+srv://maryna_sk:maryna_sk@cluster0.q2a5cgt.mongodb.net/db_contacts?retryWrites=true&w=majority"
-  )
+  .connect(DB_HOST)
   .then(() => {
     app.listen(PORT, () => {
       console.log(`Example app listening on port ${PORT}!`);
