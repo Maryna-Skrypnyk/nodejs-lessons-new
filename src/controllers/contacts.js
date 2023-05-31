3f75073527e5a40844e698161bd577e99afdbc87
Maryna Skrypnyk <74303774+Maryna-Skrypnyk@users.noreply.github.com>
2023-05-31 14:13:17 +0300

    adds pagination and status query

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -3,10 +3,25 @@ const { HttpError, ctrlWrapper } = require("../../helpers");
 
 const getContacts = async (req, res) => {
   const { _id: owner } = req.user;
-  const contacts = await Contact.find({ owner }).populate(
-    "owner",
-    "name email"
-  );
+  const { page = 1, limit = 20, favorite = null } = req.query;
+  const skip = (page - 1) * limit;
+
+  let contacts = await Contact.find({ owner }, "-createdAt -updatedAt", {
+    skip,
+    limit,
+  }).populate("owner", "name email");
+
+  if (favorite !== null) {
+    contacts = await Contact.find(
+      { owner, favorite },
+      "-createdAt -updatedAt",
+      {
+        skip,
+        limit,
+      }
+    ).populate("owner", "name email");
+  }
+
   res.json({
     status: "success",
     code: 200,
